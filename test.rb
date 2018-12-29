b491d3c0ad970c4647ad7c4080094a18f1ac756d
Adnan Hemani <adnan.h@berkeley.edu>
2018-12-29 17:13:18 -0600

    fixed tests locally

--- test.rb
+++ test.rb
@@ -2,63 +2,66 @@ require "test/unit"
  
 class TestLogger < Test::Unit::TestCase
 
+    def setup
+        `rm -r ./planning-poker/.log_cs169`
+        @dir = "./planning-poker/.log_cs169"
+        sleep 10
+    end
+
+    def teardown
+        `rm -r ./planning-poker/.log_cs169`
+    end
+
     def test_a_file_created_testenv
         puts "Starting File Created - Test Environment"
-        `rm -r ./planning-poker/.log_cs169`
         `cd planning-poker; RUBYOPT="-W0" bundle exec rspec; cd ..`
-        dir = "./planning-poker/.log_cs169"
-        assert(File.directory?(dir))
-        files = Dir.foreach(dir).select { |x| File.file?("#{dir}/#{x}") }
+        assert(File.directory?(@dir))
+        files = Dir.foreach(@dir).select { |x| File.file?("#{@dir}/#{x}") }
         puts files
         assert_equal(1, files.count)
 
         file = files[0]
-        file_str = IO.read("#{dir}/#{file}")
+        file_str = IO.read("#{@dir}/#{file}")
         assert(file_str.match(/cs169_testing, test\n.+\n--\n.+/m))
-        `rm -r ./planning-poker/.log_cs169`
     end
 
-    def test_e_adds_to_same_folder
+    def test_d_adds_to_same_folder
         puts "Starting Logs add to same folder"
-        `rm -r ./planning-poker/.log_cs169`
-        dir = "./planning-poker/.log_cs169"
+        sleep 10
+        puts Time.now.getutc.to_i
         `cd planning-poker; RUBYOPT="-W0" bundle exec rspec; cd ..`
-        assert(File.directory?(dir))
-        files = Dir.foreach(dir).select { |x| File.file?("#{dir}/#{x}") }
+        puts Time.now.getutc.to_i
+        assert(File.directory?(@dir))
+        files = Dir.foreach(@dir).select { |x| File.file?("#{@dir}/#{x}") }
+        puts files
+        puts `cat #{@dir}/#{files[0]}`
+        puts `cat #{@dir}/#{files[1]}`
         assert_equal(1, files.count)
         `cd planning-poker; RUBYOPT="-W0" bundle exec rspec; cd ..`
-        assert(File.directory?(dir))
-        files = Dir.foreach(dir).select { |x| File.file?("#{dir}/#{x}") }
+        assert(File.directory?(@dir))
+        files = Dir.foreach(@dir).select { |x| File.file?("#{@dir}/#{x}") }
         assert_equal(2, files.count)
-        `rm -r ./planning-poker/.log_cs169`
     end
 
     def test_b_file_name
         puts "Starting Filenames are of a certain pattern"
-        `rm -r ./planning-poker/.log_cs169`
-        dir = "./planning-poker/.log_cs169"
         `cd planning-poker; RUBYOPT="-W0" bundle exec rspec; cd ..`
-        files = Dir.foreach(dir).select { |x| File.file?("#{dir}/#{x}") }
+        files = Dir.foreach(@dir).select { |x| File.file?("#{@dir}/#{x}") }
         file = files[0]
         assert(file.match(/(\d{10})_([a-z0-9]{40}).txt/))
-        `rm -r ./planning-poker/.log_cs169`
     end
 
     def test_c_correct_file_content
         puts "Starting Files have a certain pattern"
-        `rm -r ./planning-poker/.log_cs169`
-        dir = "./planning-poker/.log_cs169"
         `cd planning-poker; RUBYOPT="-W0" bundle exec rspec; cd ..`
-        files = Dir.foreach(dir).select { |x| File.file?("#{dir}/#{x}") }
+        files = Dir.foreach(@dir).select { |x| File.file?("#{@dir}/#{x}") }
         file = files[0]
-        file_str = IO.read("#{dir}/#{file}")
+        file_str = IO.read("#{@dir}/#{file}")
         assert(file_str.match(/[a-z0-9_]+, [a-z]+\n.+\n--\n.+/m))
-        `rm -r ./planning-poker/.log_cs169`
     end
 
-    def test_d_file_created_devenv
+    def test_e_file_created_devenv
         puts "Starting File Created - Dev Environment"
-        `rm -r ./planning-poker/.log_cs169`
         pid = fork do 
             exec "cd planning-poker; rails server"
         end
@@ -69,9 +72,8 @@ class TestLogger < Test::Unit::TestCase
         f.close
         puts "changed file 1"
         sleep 10
-        dir = "./planning-poker/.log_cs169"
-        assert(File.directory?(dir))
-        files = Dir.foreach(dir).select { |x| File.file?("#{dir}/#{x}") }
+        assert(File.directory?(@dir))
+        files = Dir.foreach(@dir).select { |x| File.file?("#{@dir}/#{x}") }
         puts "assertion file 1"
         puts files
         puts `cat ./planning-poker/.log_cs169/#{files[0]}`
@@ -83,8 +85,8 @@ class TestLogger < Test::Unit::TestCase
         puts "changed file 2"
         sleep 10
 
-        assert(File.directory?(dir))
-        files = Dir.foreach(dir).select { |x| File.file?("#{dir}/#{x}") }
+        assert(File.directory?(@dir))
+        files = Dir.foreach(@dir).select { |x| File.file?("#{@dir}/#{x}") }
         puts "assertion file 2"
         puts files
         puts `cat ./planning-poker/.log_cs169/#{files[0]}`
@@ -94,10 +96,8 @@ class TestLogger < Test::Unit::TestCase
         Process.kill("HUP", pid)
 
         file = files[0]
-        file_str = IO.read("#{dir}/#{file}")
+        file_str = IO.read("#{@dir}/#{file}")
         assert(file_str.match(/cs169_testing, development\n.+\n--\n.+/m))     
-
-        `rm -r ./planning-poker/.log_cs169`
     end
 
 end
