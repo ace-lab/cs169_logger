41c779c773e46e8d412502d683c03f092e331c1b
Adnan Hemani <adnan.h@berkeley.edu>
2018-12-29 02:20:44 -0600

    finished all testing locally

--- test.rb
+++ test.rb
@@ -44,6 +44,37 @@ class TestLogger < Test::Unit::TestCase
     end
 
     def test_file_created_devenv
+        pid = fork do 
+            exec "cd planning-poker; rails server"
+        end
+
+        sleep 10
+        f = File.new('./planning-poker/app/models/abcd.rb', 'w')
+        f.write("puts 'abcd'\n")
+        f.close
+        puts "changed file 1"
+        sleep 3
+        dir = "./planning-poker/.log_cs169"
+        assert(File.directory?(dir))
+        files = Dir.foreach(dir).select { |x| File.file?("#{dir}/#{x}") }
+        puts "assertion file 1"
+        assert_equal(1, files.count)
+
+        f = File.new('./planning-poker/app/models/efgh.rb', 'w')
+        f.write("puts 'efgh'\n")
+        f.close
+        puts "changed file 2"
+        sleep 3
+
+        files = Dir.foreach(dir).select { |x| File.file?("#{dir}/#{x}") }
+        puts "assertion file 2"
+        assert_equal(2, files.count)
+
+        Process.kill("HUP", pid)
+
+        file = files[0]
+        file_str = IO.read("#{dir}/#{file}")
+        assert(file_str.match(/cs169_testing, development\n.+\n--\n.+/m))     
 
     end
 
